d5db05a3e6701b61900d318a3a01818e4901f1c1
Piotr Kowalski <piotr@pkowalski.net>
2022-08-19 23:45:19 +0200

    Add subscriber xmldoc

--- Rinkudesu.Kafka.Dotnet.Base/IKafkaSubscriber.cs
+++ Rinkudesu.Kafka.Dotnet.Base/IKafkaSubscriber.cs
@@ -5,9 +5,24 @@ namespace Rinkudesu.Kafka.Dotnet.Base;
 /// </summary>
 public interface IKafkaSubscriber<T> : IAsyncDisposable where T : GenericKafkaMessage
 {
+    /// <summary>
+    /// Subscribes to a topic defined by <paramref name="newHandler"/>
+    /// </summary>
+    /// <remarks>
+    /// Subscription by itself doesn't start message handling. For that, you need to call <see cref="BeginHandle"/>.
+    /// </remarks>
     void Subscribe(IKafkaSubscriberHandler<T> newHandler);
+    /// <summary>
+    /// Removes the subscription and stops message handling.
+    /// </summary>
     Task<bool> Unsubscribe();
 
+    /// <summary>
+    /// Starts message handling.
+    /// </summary>
     void BeginHandle(CancellationToken cancellationToken);
+    /// <summary>
+    /// Cancels message handling and waits for the processing to finish.
+    /// </summary>
     Task StopHandle();
 }
